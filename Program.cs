435ba4ce8c2801af19e003de77929072b1cc8b9a
KKaviya1 <kaviyagandhi136@gmail.com>
2023-07-11 12:10:40 +0530

    my commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gotoex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder stringBuilder = new StringBuilder("Welcome");
+            stringBuilder.AppendLine("to C#");
+            stringBuilder.AppendLine("I am string builder");
+            Console.WriteLine(stringBuilder);
+            Console.ReadKey();
+        }
+      
+    }
+}
